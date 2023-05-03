227d108496b79a175dacc893f6befa34c8fa3aec
rakib2023 <mdrakibulhaider.int@gmail.com>
2023-05-03 13:07:48 +0600

    installed laravel jetstream

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('students', StudentList::class);
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Route::get('students', StudentList::class)->middleware('auth');
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
